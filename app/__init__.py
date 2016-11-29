79c55eb10ad41c9f25bcf33c20786362c13c8d4f
joyshen06 <joyshen06@gmail.com>
2016-11-30 00:02:59 +0800

    Add some test data query function

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 app = Flask(__name__)
-
+# flask_cors: Cross Origin Resource Sharing (CORS), making cross-origin AJAX possible.
 app.config['DEBUG'] = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-
+CORS(app)
 from app.routes import index
 
--- app/routes/index.py
+++ app/routes/index.py
@@ -1,6 +1,28 @@
 from app import app
+import json
 
-@app.route('/')
-def root():
-    return app.send_static_file('index.html')
+DATAPATH = 'test_data/'
+
+@app.route('/stationConfig')
+def getStationConfig():
+    """This is a function to return the station configuration data
+
+    :return: the stationconfig file(json format)
+    """
+    print('tesing')
+    with open(DATAPATH+'full_station_config.json', 'r') as rf:
+        stationConfig = json.load(rf)
+    return json.dumps(stationConfig)
+
+@app.route('/windConstraint')
+def getWindConstraint():
+    """ Temp and test function
+
+    :return:,,,
+    """
+    with open(DATAPATH+'all_constraints_2.json', 'r') as rf:
+        windConstraint = json.load(rf)
+    return json.dumps(windConstraint)
+
+
 
