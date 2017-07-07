9a5708c7d3310dbee1cdb90d93194b24ea847213
qmShen <joyshen06@gmail.com>
2017-07-07 12:24:53 +0800

    svae

--- app/routes/index.py
+++ app/routes/index.py
@@ -21,7 +21,23 @@ def getStationConfig():
     return json.dumps(data)
 
 
+@app.route('/getallrecordsofstation', methods=['POST'])
+def getRecordOfStations():
 
+    post_data = json.loads(request.data.decode())
+    station_id = post_data['stationId']
+    start_time = post_data['start_time']
+    end_time = post_data['end_time']
+    aqi_attr = post_data['attr']
+    hour_range = post_data['hour_range']
+    query_result = data_service.get_records_from_time_range(
+        station_id=station_id,
+        start_time=start_time,
+        end_time=end_time,
+        data_attr=aqi_attr,
+        hour_range=hour_range
+    )
+    return json.dumps(query_result)
 
 if __name__ == '__main__':
     with open('../../test_data/'+'full_station_config.json', 'r') as rf:
